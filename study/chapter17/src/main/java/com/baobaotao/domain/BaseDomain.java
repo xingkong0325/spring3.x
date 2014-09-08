31153fd32a8c438032dddac8b133dc7dfe3fbd8d
xingkong0325 <xingkong0325@126.com>
2014-09-08 22:12:02 +0800

    20140908 home

--- study/chapter17/src/main/java/com/baobaotao/domain/BaseDomain.java
+++ study/chapter17/src/main/java/com/baobaotao/domain/BaseDomain.java
@@ -4,15 +4,21 @@ import java.io.Serializable;
 
 import org.apache.commons.lang.builder.ToStringBuilder;
 
-public class BaseDomain implements Serializable {
-
-	/**
+/**
+ * 
+ *<br><b>类描述:</b>
+ *<pre>所示PO的父类</pre>
+ *@see
+ *@since
+ */
+public class BaseDomain implements Serializable
+{
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 5736172532717316494L;
-	
-	public String toString(){
-		return ToStringBuilder.reflectionToString(this);
-	}
+	private static final long serialVersionUID = -6038001599431276800L;
 
+	public String toString() {
+        return ToStringBuilder.reflectionToString(this);
+    }
 }
